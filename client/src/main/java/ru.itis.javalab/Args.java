8a6418c39759add6ee6512611e63e647db948d0d
Morozova Aleksandra <sashamorozova01@mail.ru>
2020-12-02 22:02:59 +0300

    finish

---
+++ client/src/main/java/ru.itis.javalab/Args.java
@@ -0,0 +1,14 @@
+package ru.itis.javalab;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(separators = "=")
+public class Args {
+
+    @Parameter(names = {"--server-ip"}, required = true)
+    public String host;
+
+    @Parameter(names = {"--server-port"})
+    public Integer port;
+}
